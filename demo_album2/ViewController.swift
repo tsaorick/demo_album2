1f8118a171ac2402331d433108bfbcd018da6b89
Tsaorick <tsaorick@TsaorickdeMacBook-Pro.local>
2016-10-30 21:15:23 +0800

    Initial Commit

---
+++ demo_album2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  demo_album2
+//
+//  Created by Tsaorick on 2016/10/30.
+//  Copyright © 2016年 Tsaorick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
